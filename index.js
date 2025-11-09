09699a6a8e31c793ac3cc4664692cc7b57071499
Md Amjad hussen <mdamjadhussen1@gmail.com>
2025-11-09 10:03:12 +0600

    mongodb database connect succesfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -8,10 +9,35 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = "mongodb+srv://simpleDBUsers:TIKd5RWK37CIxWsK@cluster0.4wysv8m.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World! this is server')
+    res.send('Hello World! this is server')
 })
 
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.listen(port, () => {
-  console.log(`HomeHero is listening on port ${port}`)
+    console.log(`HomeHero is listening on port ${port}`)
 })
